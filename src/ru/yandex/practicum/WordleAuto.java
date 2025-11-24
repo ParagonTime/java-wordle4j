41a8874a6ae47d87dc21969814210049007b9872
ParagonTime <fonmaterhorn@yandex.ru>
2025-11-24 11:10:22 +0300

    feat: fix WordleAuto getAnswerWord

--- src/ru/yandex/practicum/WordleAuto.java
+++ src/ru/yandex/practicum/WordleAuto.java
@@ -20,6 +20,9 @@ public class WordleAuto {
     public String getAnswerWord() {
         boolean isSuitable = false;
         String autoAnswer = "гонец";
+        if (words.isEmpty()) {
+            return autoAnswer;
+        }
         while (!isSuitable) {
             autoAnswer = words.get((int) (Math.random() * 112342) % words.size());
             isSuitable = checkSuitable(autoAnswer);
